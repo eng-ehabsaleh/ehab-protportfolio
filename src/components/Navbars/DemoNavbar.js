87a3b0fbecf504a40da1e87b050708227b023ec3
ehab-ahma <ehab24495@gmail.com>
2021-09-02 17:40:58 +0300

    updating portfolio

--- src/components/Navbars/DemoNavbar.js
+++ src/components/Navbars/DemoNavbar.js
@@ -79,6 +79,7 @@ class DemoNavbar extends React.Component {
                   alt="..."
                   src={reactWhite}
                 />
+               
               </NavbarBrand>
               <button className="navbar-toggler" id="navbar_global">
                 <span className="navbar-toggler-icon" />
@@ -176,7 +177,7 @@ class DemoNavbar extends React.Component {
                   <UncontrolledDropdown nav>
                     <DropdownToggle nav>
                       <i className="ni ni-collection d-lg-none mr-1" />
-                      <span className="nav-link-inner--text">Examples</span>
+                      <span className="nav-link-inner--text">Menu</span>
                     </DropdownToggle>
                     <DropdownMenu>
                       <DropdownItem to="/landing-page" tag={Link}>
@@ -185,12 +186,12 @@ class DemoNavbar extends React.Component {
                       <DropdownItem to="/profile-page" tag={Link}>
                         Profile
                       </DropdownItem>
-                      <DropdownItem to="/login-page" tag={Link}>
+                      {/* <DropdownItem to="/login-page" tag={Link}>
                         Login
                       </DropdownItem>
                       <DropdownItem to="/register-page" tag={Link}>
                         Register
-                      </DropdownItem>
+                      </DropdownItem> */}
                     </DropdownMenu>
                   </UncontrolledDropdown>
                 </Nav>
@@ -198,7 +199,7 @@ class DemoNavbar extends React.Component {
                   <NavItem>
                     <NavLink
                       className="nav-link-icon"
-                      href="https://www.facebook.com/creativetim"
+                      href="https://web.facebook.com/ehab.ahmad.940641"
                       id="tooltip333589074"
                       target="_blank"
                     >
@@ -211,10 +212,10 @@ class DemoNavbar extends React.Component {
                       Like us on Facebook
                     </UncontrolledTooltip>
                   </NavItem>
-                  <NavItem>
+                  {/* <NavItem>
                     <NavLink
                       className="nav-link-icon"
-                      href="https://www.instagram.com/creativetimofficial"
+                      href="/"
                       id="tooltip356693867"
                       target="_blank"
                     >
@@ -226,11 +227,11 @@ class DemoNavbar extends React.Component {
                     <UncontrolledTooltip delay={0} target="tooltip356693867">
                       Follow us on Instagram
                     </UncontrolledTooltip>
-                  </NavItem>
+                  </NavItem> */}
                   <NavItem>
                     <NavLink
                       className="nav-link-icon"
-                      href="https://twitter.com/creativetim"
+                      href="https://twitter.com/Ehab4726?s=09"
                       id="tooltip184698705"
                       target="_blank"
                     >
@@ -240,13 +241,13 @@ class DemoNavbar extends React.Component {
                       </span>
                     </NavLink>
                     <UncontrolledTooltip delay={0} target="tooltip184698705">
-                      Follow us on Twitter
+                      Follow me on Twitter
                     </UncontrolledTooltip>
                   </NavItem>
                   <NavItem>
                     <NavLink
                       className="nav-link-icon"
-                      href="https://github.com/creativetimofficial/argon-design-system-react"
+                      href="https://github.com/eng-ehabsaleh"
                       id="tooltip112445449"
                       target="_blank"
                     >
@@ -259,11 +260,11 @@ class DemoNavbar extends React.Component {
                       Star us on Github
                     </UncontrolledTooltip>
                   </NavItem>
-                  <NavItem className="d-none d-lg-block ml-lg-4">
+                  {/* <NavItem className="d-none d-lg-block ml-lg-4">
                     <Button
                       className="btn-neutral btn-icon"
                       color="default"
-                      href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-navbar"
+                      href="/"
                       target="_blank"
                     >
                       <span className="btn-inner--icon">
@@ -273,7 +274,7 @@ class DemoNavbar extends React.Component {
                         Download
                       </span>
                     </Button>
-                  </NavItem>
+                  </NavItem> */}
                 </Nav>
               </UncontrolledCollapse>
             </Container>
--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
 import Index from "views/Index.js";
-import Landing from "views/examples/Landing.js";
+import Landing from "https://eng-ehabsaleh.github.io/ehab-protportfolio/";
 import Login from "views/examples/Login.js";
 import Profile from "views/examples/Profile.js";
 import Register from "views/examples/Register.js";
@@ -32,23 +32,29 @@ import Register from "views/examples/Register.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact render={props => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        exact
-        render={props => <Landing {...props} />}
-      />
-      <Route path="/login-page" exact render={props => <Login {...props} />} />
-      <Route
+    {/* <Route
         path="/profile-page"
         exact
         render={props => <Profile {...props} />}
-      />
+      /> */}
+      {/* <Route path="/" exact render={props => <Index {...props} />} /> */}
+      <Route path="/" exact render={props =>  <Profile {...props} />}/>
       <Route
+        path="https://eng-ehabsaleh.github.io/ehab-protportfolio/"
+        exact
+        render={props => <Landing {...props} />}
+      />
+      {/* <Route path="/login-page" exact render={props => <Login {...props} />} /> */}
+      {/* <Route
+        path="/profile-page"
+        exact
+        render={props => <Profile {...props} />}
+      /> */}
+      {/* <Route
         path="/register-page"
         exact
         render={props => <Register {...props} />}
-      />
+      /> */}
       <Redirect to="/" />
     </Switch>
   </BrowserRouter>,
